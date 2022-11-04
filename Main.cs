d844fb560778a801787badc678f0eea438764efe
o7Moon <0167moon@gmail.com>
2022-11-04 12:31:59 -0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Il2CppSystem;
+
+[assembly: MelonInfo(typeof(BarjiKart.CheckpointView.CheckpointView), "BarjiKart.CheckpointView","1.0.0","o7Moon")]
+namespace BarjiKart.CheckpointView {
+    [HarmonyPatch]
+    public class CheckpointView : MelonMod
+    {
+        [HarmonyPatch(typeof(Barji.Tracks.TrackManager),nameof(Barji.Tracks.TrackManager.Update))]
+        [HarmonyPostfix]
+        public static void onTrackUpdate(Barji.Tracks.TrackManager __instance){
+            if (!(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.C))) return;
+            foreach (Barji.Tracks.Checkpoint c in __instance.checkpoints){
+                var m = c.GetComponent<MeshRenderer>();
+                m.enabled = true;
+            }
+        }
+    }
+}
